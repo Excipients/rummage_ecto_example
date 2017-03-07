1345d17e7a814135b498254b9de63363f9f4b54f
Adi Iyengar <aditya7iyengar@gmail.com>
2017-03-07 08:56:01 -0500

    First commit

---
+++ lib/rummage_ecto_example.ex
@@ -0,0 +1,2 @@
+defmodule RummageEctoExample do
+end
---
+++ test/rummage_ecto_example_test.exs
@@ -0,0 +1,8 @@
+defmodule RummageEctoExampleTest do
+  use ExUnit.Case
+  doctest RummageEctoExample
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
